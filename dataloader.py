ef8ca673f74b67cc5ed15c538ef115a8b93261dc
BaptisteCarmier <baptiste.carmier@epfl.ch>
2024-12-29 12:39:39 +0100

    Add R_NIR_SWIR as a param in dataloader

--- dataloader.py
+++ dataloader.py
@@ -9,7 +9,7 @@ from torchvision.transforms import functional as F
 csv_path = "canopy_height_dataset/data_split.csv"
 
 class Sentinel2(Dataset):
-    def __init__(self, csv_path, split="train", transform=None, RGB=False):
+    def __init__(self, csv_path, split="train", transform=None, RGB=False, R_NIR_SWIR=False):
         """
         Args:
             csv_path (str): Path to the CSV file containing image/mask paths and splits.
@@ -23,7 +23,8 @@ class Sentinel2(Dataset):
         self.data = self.data[self.data['split'] == split]
         
         self.transform = transform
-        self.RGB= RGB       
+        self.RGB= RGB
+        self.R_NIR_SWIR = R_NIR_SWIR       
 
 
     def __len__(self):
@@ -41,6 +42,9 @@ class Sentinel2(Dataset):
         
         if self.RGB:
             image = image[[3,2,1]] #Pour sélectionner RGB
+
+        if self.R_NIR_SWIR:
+            image = image[[3,4,5,6,7,8,10,11]] #Pour sélectionner Red, Red Edge (B05-6-7), NIR (B08-8A), SWIR (B11-12) 
 
 
         # Charger le masque (supposant une seule bande)
