18561981250f6d442e3358c7a47e69e0a869f517
BaptisteCarmier <baptiste.carmier@epfl.ch>
2025-01-07 17:00:59 +0100

    je push quelques trucs

--- Pretrainmodel.py
+++ Pretrainmodel.py
@@ -30,24 +30,31 @@ print("Output shape:", output.shape)
 # Define loss and optimizer
 criterion = nn.MSELoss(reduction = 'none')  # Or SmoothL1Loss zhre one we want
 
-validation_dl = Sentinel2(csv_path=csv_path, split="validation", RGB=True)
-validation_loader = DataLoader(validation_dl, batch_size=16, shuffle=False) #False pour permettre une comparaison entre les modèles
+test_dl = Sentinel2(csv_path=csv_path, split="test", RGB=True)
+test_loader = DataLoader(test_dl, batch_size=32, shuffle=True) #False pour permettre une comparaison entre les modèles
 
-val_loss = 0.0
+loss = 0.0
 
 model.eval()
 with torch.no_grad():
-    for batch_idx, (images, masks) in enumerate(validation_loader):
+    for batch_idx, (images, masks) in enumerate(test_loader):
         images = images.to(device)
         masks = masks.to(device)
 
         outputs = model(images)
 
-        loss = criterion(outputs, masks)
-        val_loss+=loss.item() ## on a une erreur de taille ici RuntimeError: "a Tensor with 16384 elements cannot be converted to Scalar"
+        loss_map = criterion(outputs, masks)
+        valid_mask = (masks != 255).float()
+        valid_loss_map = loss_map*valid_mask
 
-val_loss /= len(validation_loader)
-print(f"Validation Loss: {val_loss:.4f}")
+        num_valid_pixels = valid_mask.sum()
+        loss = valid_loss_map.sum() / num_valid_pixels
 
-rmse = torch.sqrt(torch.tensor(val_loss))
-print(f"Validation rmse: {rmse:.4f}")
+        #loss = criterion(outputs, masks)
+        #val_loss+=loss.item() ## on a une erreur de taille ici RuntimeError: "a Tensor with 16384 elements cannot be converted to Scalar"
+
+loss /= len(test_loader)
+print(f"Test Loss: {loss:.4f}")
+
+rmse = torch.sqrt(torch.tensor(loss))
+print(f"Test rmse: {rmse:.4f}")
